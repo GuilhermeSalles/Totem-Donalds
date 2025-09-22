0ba3e2cbc014f6b66c6b5eef39d6225ea9c9a812
GuilhermeSalles <guibaltazarvs@gmail.com>
2025-09-22 14:25:43 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-const ProductsPage = () => {
+const ProductPage = () => {
   return (
-    <div className="p-4 border-red-500 rounded-xl">
-      <h1 className="text-2xl font-bold ">Products</h1>
-      <Button>Click Me</Button>
-      <Input placeholder="Enter text" />
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">products page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Bora fechar esse projeto!" />
     </div>
   );
 };
 
-export default ProductsPage;
+export default ProductPage;
